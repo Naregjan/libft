95da9cab782a3c64fe06a1d9c0713aba497589fa
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-03-11 03:11:22 -0700

    fixed misnamed variable

--- ft_findintab.c
+++ ft_findintab.c
@@ -6,7 +6,7 @@
 /*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 02:51:34 by ikourkji          #+#    #+#             */
-/*   Updated: 2019/03/11 02:56:49 by ikourkji         ###   ########.fr       */
+/*   Updated: 2019/03/11 03:10:34 by ikourkji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int		ft_findintab(char **haystack, char *needle)
 	int i;
 
 	i = 0;
-	while (tab[i])
+	while (haystack[i])
 	{
-		if (ft_strcmp(tab[i], needle) == 0)
+		if (ft_strcmp(haystack[i], needle) == 0)
 			return (i);
 		i++;
 	}
