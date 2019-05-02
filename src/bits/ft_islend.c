ce03e525588073e488ba889b88636bdf8f3793ea
naregjan <iskandar.kourkjian.mowad@gmail.com>
2019-05-02 05:35:04 -0700

    simplified swapend, added endianness checker

--- src/bits/ft_islend.c
+++ src/bits/ft_islend.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swapend32.c                                     :+:      :+:    :+:   */
+/*   ft_islend.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikourkji <ikourkji@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/11 02:21:02 by ikourkji          #+#    #+#             */
-/*   Updated: 2019/05/02 05:26:18 by ikourkji         ###   ########.fr       */
+/*   Created: 2019/05/02 05:30:18 by ikourkji          #+#    #+#             */
+/*   Updated: 2019/05/02 05:33:21 by ikourkji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** swaps endianness for 32-bit vals
+** "is little endian": I know there's a whole "is" folder,
+** but this is about bits dammit.
+** tests machine bit storage.
+** 1 = Little Endian, 2 = Big Endian
 */
 
-uint32_t	ft_swapend32(uint32_t n)
+int	ft_islend(void)
 {
-	uint32_t swapped;
+	short int	word;
+	char		*b;
 
-	swapped = (n & 0x000000ff) << 24u;
-	swapped |= (n & 0x0000ff00) << 8u;
-	swapped |= (n & 0x00ff0000) >> 8u;
-	swapped |= (n & 0xff000000) >> 24u;
-	return (swapped);
+	word = 0x0001;
+	b = (char*)&word;
+	return(b[0] ? 1 : 0);
 }
